dbbcb457f4486077d2cdc1e29297be24aa1848e5
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-05-01 11:11:54 -0700

    Adds Navbar and Notices partials

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Trampoline
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.application_name = "Trampoline"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
