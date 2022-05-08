f8bf6c1b9b830003058aeef74373d08d31d54ee3
Robert <16901597+aquaflamingo@users.noreply.github.com>
2022-05-08 09:34:55 -0700

    Adds bootstrap, styling and other fun stuff (#4)
    * Create AddCompletedAtToRuns migration
    
    * [MIGRATION]
    
    * Adds download button for result of run
    
    * Adds completed_at to Job run
    
    * Adds styling and formatting to index and show pages
    
    * updates gitignore
    
    * Git Ignore
    
    * Fix Docker and Compose bind mounts
    
    * Adds Navbar and Notices partials
    
    * Fixes syntax error in jobs_controller
    
    * Adds various style improvements

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,40 @@ class Job < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+
+  def complete?
+    run.present? && run.completed_at
+  end
+
+  def to_downloadable
+    d = Download.new(self)
+
+    d.result
+  end
+
+  class Download
+    ERBPayload = Struct.new(:name, :description, :text)
+
+    def initialize(j)
+      @payload = ERBPayload.new(
+        j.title,
+        j.description,
+        j.run&.resolved_text
+      )
+    end
+
+    def result
+      erb_template_fp = Rails.root.join("app", "views", "jobs", "download.erb.txt")
+
+      erb = ERB.new(File.read(erb_template_fp))
+
+      erb.result(get_binding)
+    end
+
+    # disable:rubocop
+    def get_binding
+      binding
+    end
+    # enable:rubocop
+  end
 end
---
+++ db/migrate/20220416012231_add_completed_at_to_runs.rb
@@ -0,0 +1,5 @@
+class AddCompletedAtToRuns < ActiveRecord::Migration[7.0]
+  def change
+    add_column :runs, :completed_at, :datetime
+  end
+end
