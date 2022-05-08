53e4e18c9716a36883794e8a0aa01602601a5151
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-05-08 10:43:48 -0700

    Configures GCP and OpenAI clients to use profiles

--- config/initializers/google_cloud_vision.rb
+++
@@ -1,6 +0,0 @@
-Bundler.require 
-require "google/cloud/vision"
-
-Google::Cloud::Vision.configure do |c|
-  c.credentials = Rails.application.credentials.gcp
-end
--- config/initializers/openai.rb
+++
@@ -1,5 +0,0 @@
-require "#{Rails.root}/app/lib/openai"
-
-OpenAI::Config.configure do |c|
-  c.access_token = Rails.application.credentials.openai.access_token
-end
