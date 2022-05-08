7e3ae98a06fe445dcafc40d904b050733c6d2dfe
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-05-08 11:25:52 -0700

    Adds Secrets new, index, destroy, actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  def current_profile?
+    current_profile.present?
+  end
+
+  def current_profile
+    # TODO 
+    if session[:profile_id]
+      @current_profile = Profile.find(session[:profile_id])
+      @current_profile
+    else 
+      # FIXME:
+      Profile.first
+    end
+  end
 end
